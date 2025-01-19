0e01f6731cbe8e21b1d919a0eabc5c3cabba4426
vic5970 <vic5970@gmail.com>
2025-01-19 12:20:59 +0500

    eu-bl

---
+++ a-date/backredunder-eu.js
@@ -0,0 +1,38 @@
+// Backunder Luck [DK/FI/NO/SE] OFFER ADULT-DATING
+var back = 'https://a.vivawins.com/visit/5a58e058-c985-4634-82d4-dd97bef6b880?track=BackButton';
+var under = 'https://a.vivawins.com/visit/3dde9179-49f6-4f42-aeaf-b0a683cb04e7?track=Under';
+
+if (typeof under !== "undefined" && under !== null) {
+    document.addEventListener('click', function(event) {
+        const target = event.target.closest('a'); // Современный подход
+        if (target) {
+            const linkHref = target.href;
+            event.preventDefault();
+            window.open(linkHref, '_blank'); // Открытие в новой вкладке
+            window.location.replace(under);  // Замена текущей страницы
+        }
+    });
+}
+
+if (typeof back !== "undefined" && back !== null) {
+    (function() {
+        try {
+            const URL = window.location.href.split(/[#]/)[0];
+            for (let t = 0; t < 10; t++) history.pushState({}, '', URL + '#');
+            window.onpopstate = function(event) {
+                if (event.state) location.replace(back);
+            };
+        } catch (error) {
+            console.error(error);
+        }
+    })();
+}
+
+// Redirect Function
+function redirectAfterDelay() {
+    const redirectUrl = "https://a.vivawins.com/click";
+    setTimeout(() => {
+        window.location.href = redirectUrl;
+    }, 180000); // 3 минуты
+}
+window.addEventListener('load', redirectAfterDelay);
